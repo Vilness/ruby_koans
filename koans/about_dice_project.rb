ff583c0183bc7c4a80ca270b372b571e2e438f12
Иван Чубенко <ivan_0572@list.ru>
2024-04-21 14:25:00 +0300

    MODIFIED: about_dice_project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  def roll(size)
+    @values = (1..size).map { |item| rand(6) + 1 }
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
